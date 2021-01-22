1e6779693677524f1408bc6ce3823ac2aaeada1a
Emmanuel <ortizdeluna74@gmail.com>
2021-01-21 19:30:28 -0600

    Validaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'crearcuenta',
     loadChildren: () => import('./componentes/crearcuenta/crearcuenta.module').then( m => m.CrearcuentaPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./componentes/login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/componentes/crearcuenta/crearcuenta.page.ts
+++ src/app/componentes/crearcuenta/crearcuenta.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-crearcuenta',
   templateUrl: './crearcuenta.page.html',
@@ -8,24 +9,28 @@ import { environment } from 'src/environments/environment';
 })
 export class CrearcuentaPage implements OnInit {
   sitekey= "";
-  nombre: string;
-  correoElectronico: string;
-  isvalid= false;
+  nombre= "";
+  correoElectronico= "";
+  estado= "";
+  ciudad= "";
+  telefono= "";
+  password= "";
+  confirmacion= "";
+  captcha= false;
   correoValidacion= "";
   valor= "";
   bool:boolean;
-    constructor() {
+    constructor(private router: Router) {
       this.sitekey = environment.recaptcha.siteKey;
   
     }
   ngOnInit() {
   }
   resolved(ev){
-    this.isvalid = !this.isvalid;
-    console.log(this.isvalid);
+    this.captcha = !this.captcha;
+    console.log(this.captcha);
   } 
   validarCorreo(){
-  
     //let valor = 
       if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(this.correoElectronico)){
        this.correoValidacion = "La dirección de email es valida.";
@@ -35,6 +40,85 @@ export class CrearcuentaPage implements OnInit {
        this.bool = false;
       }
     
+  }
+  async AlertaExito(){
+    Swal.fire({
+      
+      icon: 'success',
+      title: 'Listo',
+      text: 'Tu cuenta se ha creado con exito',
+      showConfirmButton: false,
+      timer: 2000
+    })
+  }
+
+  ErrorAlert(mensaje){
+    Swal.fire({
+      icon: 'info',
+      title: '!Atención¡',
+      text: mensaje,
+      footer: 'FASTORE'
+    })
+  }
+  validacionCuenta(){
+    console.log('ebtro');
+    if(this.nombre === ""){
+    console.log(1);
+    this.ErrorAlert('Falta ingresar tu nombre');
+    }else{
+      if(this.correoElectronico === ""){
+        console.log(2);
+        this.ErrorAlert('Falta ingresar tu Correo Electronico');
+      }else{
+        if(this.estado === ""){
+          console.log(3);
+          this.ErrorAlert('Falta ingresar tu Estado');
+        }else{
+          if(this.ciudad === ""){
+            console.log(4);
+            this.ErrorAlert('Falta ingresar tu Ciudad');
+          }else{
+            if(this.telefono === ""){
+              console.log(5);
+              this.ErrorAlert('Falta ingresar tu telefono');
+            }else{
+              if(this.password === ""){
+                console.log(6);
+                this.ErrorAlert('Falta ingresar tu Contraseña');
+              }else{
+              if(this.confirmacion === ""){
+                console.log(7);
+                this.ErrorAlert('Falta confirmar tu contraseña');
+              }else{
+                if(!this.captcha){
+                  console.log(8);
+                  this.ErrorAlert('Falta rellenar el Captcha');
+                }else{
+                  if(this.bool){
+                   
+                    if(this.password === this.confirmacion){
+                      console.log('ok');
+
+                      this.AlertaExito().then((res)=>{
+                      this.router.navigate(['/home']);
+                    });
+                    }else{
+                      this.ErrorAlert('La contraseña es incorrecta');
+
+                    }
+                    
+                  }else{
+                    this.ErrorAlert('El correo es invalido');
+
+                  }
+                }
+              }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
 }
